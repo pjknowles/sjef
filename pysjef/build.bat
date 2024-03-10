0b74033af39cb6c53593f64f5ec89dc95eb2a7c0
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-03-10 18:44:23 +0000

    windows rsync

--- pysjef/build.bat
+++ pysjef/build.bat
@@ -1,10 +1,10 @@
-call conda install -y -c conda-forge --file requirements.txt m2-base m2-ssh
+call conda install -y -c conda-forge --file requirements.txt
 set PREFIX=%CONDA_PREFIX%
 rmdir /S /Q cmake-build
 mkdir cmake-build
 cd cmake-build
 cmake -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% -DCMAKE_BUILD_TYPE=Release ..\..
-cmake --build . -t install --config Release
+cmake --build . -t install --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . -t _version.py
 move _version.py ..\pysjef
 cd ..
