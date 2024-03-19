94fe8bd6f17b3cc11aed717523df3b3e9ba85a9a
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-03-19 15:16:06 +0000

    windows rsync

--- pysjef/setup_msys.bat
+++ pysjef/setup_msys.bat
@@ -1,3 +1,3 @@
-rem conda install -y m2-curl
-curl https://github.com/msys2/msys2-installer/releases/download/2024-01-13/msys2-x86_64-20240113.exe
+rem conda install -y m2-wget
+wget -O msys2.exe https://github.com/msys2/msys2-installer/releases/download/2024-01-13/msys2-x86_64-20240113.exe
 .\msys2.exe install --root C:\msys2-try --confirm-command
